b8d5dcc40ee6799333ee41016e69bc4569e6f682
Phil Frilling <pfrilling@midnetmedia.com>
2014-07-01 09:29:53 -0400

    by pfrilling: Added the additionalClass variable to alow users to specify an identifying class on the menu

--- jquery.slicknav.js
+++ jquery.slicknav.js
@@ -20,6 +20,7 @@
         allowParentLinks: false,
         nestedParentLinks: true,
         showChildren: false,
+        additionalClass: false,
         init: function(){},
         open: function(){},
         close: function(){}
@@ -73,6 +74,12 @@
         // create menu bar
         $this.mobileNav.attr('class', prefix+'_nav');
         var menuBar = $('<div class="'+prefix+'_menu"></div>');
+
+        // add the additional class to the menu if available.
+        if (settings.additionalClass) {
+          menuBar.addClass(settings.additionalClass);
+        }
+
         $this.btn = $('<'+settings.parentTag+' aria-haspopup="true" tabindex="0" class="'+prefix+'_btn '+prefix+'_collapsed"><span class="'+prefix+'_menutxt">'+settings.label+'</span><span class="'+iconClass+'"><span class="'+prefix+'_icon-bar"></span><span class="'+prefix+'_icon-bar"></span><span class="'+prefix+'_icon-bar"></span></span></a>');
         $(menuBar).append($this.btn);        
         $(settings.prependTo).prepend(menuBar);
